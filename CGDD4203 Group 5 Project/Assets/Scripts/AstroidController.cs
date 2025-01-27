44a8ad2a82f6ef3d240688eb7c6d1ae18f9c51e1
Jacob M <31823746+Itsmillertyme@users.noreply.github.com>
2025-01-27 15:43:13 -0500

    Revert "Prototype GUI complete"

--- CGDD4203 Group 5 Project/Assets/Scripts/AstroidController.cs
+++ CGDD4203 Group 5 Project/Assets/Scripts/AstroidController.cs
@@ -46,16 +46,13 @@ public class AstroidController : MonoBehaviour {
             //Destroy projectile
             Destroy(other.gameObject);
 
-            //Split into smaller asteroids
+            //split into smaller asteroids
             if (size != 1) {
                 Split(2);
             }
             else {
                 Destroy(gameObject);
             }
-
-            //Update player score
-            GameObject.FindWithTag("Player").GetComponent<ShipController>().updateScore(Size);
 
         }
     }

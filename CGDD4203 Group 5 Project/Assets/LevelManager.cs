3f8f77826e8380274782ddf4ff7432e5833f6d09
hare_ware <3469405-hare_ware@users.noreply.gitlab.com>
2025-02-19 19:32:31 -0500

    Added stubbed StartNextLeg function

--- CGDD4203 Group 5 Project/Assets/LevelManager.cs
+++ CGDD4203 Group 5 Project/Assets/LevelManager.cs
@@ -11,4 +11,12 @@ public partial class LevelManager : MonoBehaviour
     {
         get => journeyLegs.Sum(l => l.ElapsedTime);
     }
+
+    public void StartNextLeg()
+    {
+        // TODO: Move the level back so the player is close to origin
+        // TODO: Add a pretty transition cutscene to hide the last thing
+        // TODO: Enable Objects in Next Leg, disable or destroy ones in current
+        throw new NotImplementedException();
+    }
 }
